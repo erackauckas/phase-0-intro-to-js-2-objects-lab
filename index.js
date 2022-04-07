54b60a78e47be3564a68b5e280d0926401a6f766
Eric Rackauckas <erackauckas@gmail.com>
2022-04-07 10:47:55 -0700

    commit tree f805970142e4be5bcb19fadb64cd3e937d98b47a

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
-// Write your solution in this file!
+// const employee = {
+//     name: "Name",
+//     streetAddress: "address",
+// };
+
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+//     return {
+//         ...employee,
+//         [key]: value,
+//     };                                                                                                       
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+//     employee[key] = value;
+//     return employee 
+// };
+
+// function deleteFromEmployeeByKey(employee) {
+//     const newEmp = {...employee} ;
+//     delete newEmp.name;
+//     return newEmp
+// };
+
+// function destructivelyDeleteFromEmployeeByKey(employee) {
+//     delete employee.name;
+//     return employee;
+// };
+
+
+
+const employee = {
+    name: "Name" ,
+    streetAddress: "address"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, [key]: value};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key,value) {
+    return {employee, [key]: value};
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
